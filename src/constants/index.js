2f0a1ad620fe6bece058ca25f9bfda031acbf245
vishal2799 <sharma.vishal2799@gmail.com>
2024-12-11 23:22:50 +0530

    breadcrumb component

--- src/constants/index.js
+++ src/constants/index.js
@@ -94,6 +94,57 @@ const ServicesTwo = [
   },
 ];
 
+const SolutionsTwo = [
+  {
+    id: 1,
+    name: 'Product Design',
+    icon: 'phone',
+    image: '/service21.jpg',
+    description:
+      'Brainstorming and generating ideas based on research findings. This phase involves sketching, creating mood boards, and exploring different concepts to develop innovative solutions.',
+  },
+  {
+    id: 2,
+    name: 'Web Development',
+    icon: 'code-slash',
+    image: '/service21.jpg',
+    description:
+      'Building responsive and scalable websites using modern frameworks and technologies, ensuring cross-browser compatibility and optimal performance.',
+  },
+  {
+    id: 3,
+    name: 'Mobile App Development',
+    icon: 'terminal',
+    image: '/service21.jpg',
+    description:
+      'Creating intuitive and user-friendly mobile applications for iOS and Android platforms with a focus on smooth performance and appealing UI/UX design.',
+  },
+  {
+    id: 4,
+    name: 'UI/UX Design',
+    icon: 'palette',
+    image: '/service21.jpg',
+    description:
+      'Designing visually appealing and user-centered interfaces that enhance user experience and ensure seamless interaction across devices.',
+  },
+  {
+    id: 5,
+    name: 'Digital Marketing',
+    icon: 'megaphone',
+    image: '/service21.jpg',
+    description:
+      'Implementing data-driven marketing strategies, including SEO, social media management, and content marketing to maximize brand visibility and reach.',
+  },
+  {
+    id: 6,
+    name: 'Branding & Identity',
+    icon: 'briefcase',
+    image: '/service21.jpg',
+    description:
+      'Creating strong brand identities through logo design, visual storytelling, and brand strategy development to build lasting customer impressions.',
+  },
+];
+
 const Projects = [
   {
     id: 1,
@@ -133,4 +184,4 @@ const Projects = [
   },
 ];
 
-export { Services, Projects, ServicesTwo };
+export { Services, Projects, ServicesTwo, SolutionsTwo };
