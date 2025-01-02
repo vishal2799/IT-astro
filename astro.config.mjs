3bb20fddb92dbb924070095b87a14d2d48044077
vishal2799 <sharma.vishal2799@gmail.com>
2025-01-02 18:29:37 +0530

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  site: 'https://alicornsolutions.com',
+  integrations: [tailwind(), icon(), sitemap()],
 });
